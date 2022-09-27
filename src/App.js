8ab4e6870a32d8bd8afaf4069f98da0964f385eb
Will <wmchance@gmail.com>
2022-09-27 17:12:06 +0700

    Add static PhotoContainer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MainNav from "./components/MainNav";
 import SearchForm from "./components/SearchForm";
+import PhotoContainer from "./components/PhotoContainer";
 
 function App() {
   return (
     <div className="container">
       <SearchForm />
       <MainNav />
+      <PhotoContainer />
     </div>
   );
 }
