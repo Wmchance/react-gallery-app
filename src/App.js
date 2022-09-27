65e1bb4017113a6d90c177c919a4c26155215255
Will <wmchance@gmail.com>
2022-09-27 17:02:14 +0700

    Add static SearchForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MainNav from "./components/MainNav";
+import SearchForm from "./components/SearchForm";
 
 function App() {
   return (
     <div>
+      <SearchForm />
       <MainNav />
     </div>
   );
