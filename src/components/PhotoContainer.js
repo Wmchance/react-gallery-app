38baac8a6f3a0aebe4c5ac8e2125ceb89325e1cc
Will <wmchance@gmail.com>
2022-09-27 17:17:23 +0700

    Update 'No results' message

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -20,7 +20,7 @@ const PhotoContainer = () => {
              {/* Not Found */}
             <li className="not-found">
                 <h3>No Results Found</h3>
-                <p>You search did not return any results. Please try again.</p>
+                <p>Your search did not return any results. Please try again.</p>
             </li>
             </ul>
         </div>
