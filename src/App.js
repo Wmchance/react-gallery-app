ca0afac10a776cf06c5a72931291ce97ca1b5a44
Will <wmchance@gmail.com>
2022-09-28 11:13:25 +0700

    Setup config.js w/ api key & import key to App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import MainNav from "./components/MainNav";
 import SearchForm from "./components/SearchForm";
 import PhotoContainer from "./components/PhotoContainer";
+import PhotoFetcher from "./components/PhotoFetcher";
+
+import apiKey from "./config";
+const myApiKey = apiKey;
 
 function App() {
   return (
     <div className="container">
       <SearchForm />
       <MainNav />
-      <PhotoContainer />
+      <PhotoContainer /> {/* to PhotoFetcher */}
+      <PhotoFetcher myApiKey = {myApiKey}/>
     </div>
   );
 }
---
+++ src/components/PhotoFetcher.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PhotoContainer from "./PhotoContainer";
+
+const PhotoFetcher = (props) => {
+    return (
+        <h1>{props.myApiKey}</h1>
+    )
+}
+
+export default PhotoFetcher;
