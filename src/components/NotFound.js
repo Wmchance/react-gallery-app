b7e9dff29e2707858f00f5d0693a414582e12a36
Will <wmchance@gmail.com>
2022-10-04 15:54:25 +0700

    Update 404 component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
     return(
         <div className="photo-container">
-            <h1>404</h1>
+            <h2>404</h2>
             <h3>File not found</h3>
         </div>
     )
